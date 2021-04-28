50729a579873b702e4634461b594177cd8883d8a
William Perez <william.perez@pucp.edu.pe>
2021-04-28 02:01:31 -0500

    updating analytics schema

--- src/load/transform.py
+++ src/load/transform.py
@@ -23,30 +23,41 @@ def build_dbuildings(df_source: pd.DataFrame) -> pd.DataFrame:
 def transform_data(df_source: pd.DataFrame, db_engine:sqlalchemy.engine.Engine):
     df_source['Incident Number'] = df_source['Incident Number'].astype('int32')
 
-    # Get last incident ingested
-    db_conn = db_engine.connect()
-    result = db_conn.execute('SELECT MAX("Incident Number") FROM analytics.dwh_fincidents')
     max_incident_number = 0
-    for row in result:
-        if row['max'] is not None:
-            max_incident_number = row['max']
-    print(f"Last incident number in database is: {max_incident_number}")
+    # Get last incident ingested
+    with db_engine.connect() as connection:
+        result = connection.execute('SELECT MAX("Incident Number") FROM analytics.dwh_fincidents')
+        for row in result:
+            if row['max'] is not None:
+                max_incident_number = row['max']
+        print(f"Last incident number in database is: {max_incident_number}")
 
     # Restrict dataset
     df_new_incidents = df_source.loc[df_source['Incident Number'] > max_incident_number,]
 
-    # Build datasets and upload to staging table
-    dwh_fincidents = build_fincidents(df_new_incidents)
-    print("Loading dwh_finicidents")
-    dwh_fincidents.to_sql(con=db_engine, name='dwh_fincidents', schema='staging', index=False, method='multi',
-                          dtype=sqlalchemy.types.VARCHAR(length=100), if_exists='replace', chunksize=100)
-    print("Loading dwh_dgeography")
-    dwh_dgeography = build_dgeography(df_new_incidents)
-    dwh_dgeography.to_sql(con=db_engine, name='dwh_fincidents', schema='staging', index=False, method='multi',
-                          dtype=sqlalchemy.types.VARCHAR(length=100), if_exists='replace', chunksize=100)
-    print("Loading dwh_dbuildings")
-    dwh_dbuildings = build_dbuildings(df_new_incidents)
-    dwh_dbuildings.to_sql(con=db_engine, name='dwh_fincidents', schema='staging', index=False, method='multi',
-                          dtype=sqlalchemy.types.VARCHAR(length=100), if_exists='replace', chunksize=100)
+    with db_engine.connect() as connection:
+        with connection.begin():
+            # Build datasets and upload to staging table
+            dwh_fincidents = build_fincidents(df_new_incidents)
+            print("Loading dwh_finicidents")
+            dwh_fincidents.to_sql(con=connection, name='dwh_fincidents', schema='staging', index=False, method='multi',
+                                  dtype=sqlalchemy.types.VARCHAR(length=100), if_exists='replace', chunksize=100)
+            print("Loading dwh_dgeography")
+            dwh_dgeography = build_dgeography(df_new_incidents)
+            dwh_dgeography.to_sql(con=connection, name='dwh_fincidents', schema='staging', index=False, method='multi',
+                                  dtype=sqlalchemy.types.VARCHAR(length=100), if_exists='replace', chunksize=100)
+            print("Loading dwh_dbuildings")
+            dwh_dbuildings = build_dbuildings(df_new_incidents)
+            dwh_dbuildings.to_sql(con=connection, name='dwh_fincidents', schema='staging', index=False, method='multi',
+                                  dtype=sqlalchemy.types.VARCHAR(length=100), if_exists='replace', chunksize=100)
+
+    print("Updating analytics tables")
+    with db_engine.connect() as connection:
+        with connection.begin():
+            connection.execute('INSERT INTO analytics.dwh_fincidents SELECT * FROM staging.dwh_fincidents')
+            connection.execute('INSERT INTO analytics.dwh_dgeography SELECT * FROM staging.dwh_dgeography')
+            connection.execute('INSERT INTO analytics.dwh_dbuildings SELECT * FROM staging.dwh_dbuildings')
+
+    print("Upload finished")
 
 
