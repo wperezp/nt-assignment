fb8bd21929f599b6a6f4fc6e4f84b7a3c6dad453
William Perez <william.perez@pucp.edu.pe>
2021-04-27 22:15:21 -0500

    testing csv read

--- src/load/main.py
+++ src/load/main.py
@@ -1,8 +1,14 @@
 import os
 import boto3
-import pandas
+import pandas as pd
 import psycopg2
 
 if __name__ == '__main__':
     print(os.environ)
-    
+    db_host = os.environ['DB_HOST']
+    db_user = os.environ['DB_USER']
+    db_pass = os.environ['DB_PASS']
+    conn = psycopg2.connect(host=db_host, database='postgres', user=db_user, password=db_pass)
+    s3_bucket = os.environ['S3_DATA_BUCKET']
+    df = pd.read_csv(f's3://{s3_bucket}/landing/fire_data.csv')
+    df.head(10)
