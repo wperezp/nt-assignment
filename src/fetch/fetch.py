0156efc35843ad98633ab31989596ecb81ce270b
William Perez <william.perez@pucp.edu.pe>
2021-04-28 00:31:53 -0500

    Build fact incidents

--- src/fetch/fetch.py
+++ src/fetch/fetch.py
@@ -1,7 +1,6 @@
 import boto3
 import os
 import requests
-import sqlalchemy
 
 
 def lambda_handler(event, context):
--- src/load/transform.py
+++ src/load/transform.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import sqlalchemy
+import schemas
 
 
 def build_fincidents(df_source: pd.DataFrame) -> pd.DataFrame:
-    return df_source
+    df_fincidents = df_source.loc[:, schemas.fincidents_cols]
+    df_fincidents = df_fincidents.drop_duplicates(['Incident Number'])
+    return df_fincidents
 
 
 def build_dgeography(df_source: pd.DataFrame) -> pd.DataFrame:
@@ -15,8 +18,19 @@ def build_dbuildings(df_source: pd.DataFrame) -> pd.DataFrame:
 
 
 def transform_data(df_source: pd.DataFrame, db_engine:sqlalchemy.engine.Engine):
-    dwh_fincidents = build_fincidents(df_source)
-    db_engine.execute('TRUNCATE TABLE staging.dwh_fincidents;')
+    df_source['Incident Number'] = df_source['Incident Number'].astype(int)
+
+    db_conn = db_engine.connect()
+    result = db_conn.execute('SELECT MAX("Incident Number") FROM analytics.dwh_fincidents')
+    max_incident_number = 0
+    for row in result:
+        if row['max'] is not None:
+            max_incident_number = row['max']
+    print(max_incident_number)
+
+    df_new_incidents = df_source.loc[df_source['Incident Number'] > max_incident_number,]
+
+    dwh_fincidents = build_fincidents(df_new_incidents)
     dwh_fincidents.to_sql(con=db_engine, name='dwh_fincidents', schema='staging', index=False, method='multi', dtype=str, if_exists='replace')
 
 
